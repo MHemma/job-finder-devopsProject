e923d1cd5f8c01ee7f980f17d8ed4105d3daa977
Hemma <hemma1108@gmail.com>
2024-01-15 20:13:19 +0800

    update

--- src/main/java/com/example/jobfinder/exceptions/JobAlreadySavedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.jobfinder.exceptions;
-
-public class JobAlreadySavedException extends RuntimeException {
-    public JobAlreadySavedException(Long id) {
-        super("Job with id:" + id + " is already saved.");
-    }
-}
